4c3eb0b54fd7b64a9c5421e9a02aafb61089afa7
wiktoriamiich <272519@student.pwr.edu.pl>
2025-04-09 08:30:47 +0200

    Added full project

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "MaxHeap.hpp"
+#include "HeapPriorityQueue.hpp"
+#include "include/Utilities.hpp"
+#include "include/Menu.hpp"
+
+int main()
+{
+	DataTypeMenu m1;
+	m1.run();
+
+	/*
+	measurePerformance(10000, 10000);
+	measurePerformance(10000, 20000);
+	measurePerformance(10000, 30000);
+	measurePerformance(10000, 40000);
+	measurePerformance(10000, 50000);
+	measurePerformance(10000, 60000);
+	measurePerformance(10000, 70000);
+	measurePerformance(10000, 80000);
+	measurePerformance(10000, 90000);
+	measurePerformance(10000, 100000);
+	measurePerformance(10000, 110000);
+	measurePerformance(10000, 120000);
+	measurePerformance(10000, 130000);
+	measurePerformance(10000, 140000);
+	measurePerformance(10000, 150000);
+	measurePerformance(10000, 200000);
+	measurePerformance(10000, 250000);
+	measurePerformance(10000, 300000);
+	measurePerformance(10000, 400000);
+	measurePerformance(10000, 500000);
+	*/
+
+	return 0;
+}
